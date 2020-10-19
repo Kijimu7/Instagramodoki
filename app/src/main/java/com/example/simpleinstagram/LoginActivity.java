e7da8af35d77fb4a99479ed03c464c861440f25a
Kijimj7 <pinkokinawa84@gmail.com>
2020-10-18 21:54:35 -0700

    Able to take picture and save it to the database

--- app/src/main/java/com/example/simpleinstagram/LoginActivity.java
+++ app/src/main/java/com/example/simpleinstagram/LoginActivity.java
@@ -2,14 +2,20 @@ package com.example.simpleinstagram;
 
 import android.app.Activity;
 import android.app.AppComponentFactory;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.NoCopySpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,6 +29,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if(ParseUser.getCurrentUser() != null){
+            goMainActivity();
+        }
 
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
@@ -42,6 +52,23 @@ public class LoginActivity extends AppCompatActivity {
 
 private void loginUser(String username, String password){
     Log.i(TAG, "Attempting to login user " + username);
-
+    ParseUser.logInInBackground(username, password, new LogInCallback() {
+        @Override
+        public void done(ParseUser user, ParseException e) {
+            if (e !=null){
+                Log.e(TAG, "Issue with login", e);
+                Toast.makeText(LoginActivity.this, "Issue with login ", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            //TODO: navigate to the main activity if the user has signed in properly
+            goMainActivity();
+            Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT).show();
+        }
+    });
+}
+private void goMainActivity(){
+    Intent i = new Intent(this, MainActivity.class);
+    startActivity(i);
+    finish();
 }
 }
